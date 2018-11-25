0e1fed62adc4e0ac27af25fc00671854d49afeb3
Github User Name <you@example.com>
2018-11-25 09:41:25 +0300

    innitializing my application at this commit

---
+++ config.py
@@ -0,0 +1,20 @@
+import os
+
+  
+class Config:
+    # the uri for the database
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://fred:password@localhost/blogpost'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+# 'development':DevConfig,
+'production':ProdConfig
+}
---
+++ start.sh
@@ -0,0 +1,5 @@
+export MAIL_USERNAME="fredkheisa@gmail.com"
+export MAIL_PASSWORD="musicstrings6"
+export SECRET_KEY="2233"
+
+python3.6 manage.py server
