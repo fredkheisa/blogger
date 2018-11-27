abd11a1ef95db7609f44e9c4fe0d2ff9e5a7b6ae
Github User Name <you@example.com>
2018-11-27 08:47:56 +0300

    creating the mail function ,on sign up a user should be able to receive a mail inbox

--- config.py
+++ config.py
@@ -7,6 +7,15 @@ class Config:
 
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://fred:password@localhost/blogpost'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+#  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+
 class ProdConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
